4c62fc11f6f00ea199f16eae5f048036dd3671bf
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-05-07 10:48:23 +0300

    task 4-1-5

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 2:
+                 System.out.println("28");
+            break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30");
+            break;
+            default:
+                System.out.println("31");
+            break;
+        }
     }
 }
